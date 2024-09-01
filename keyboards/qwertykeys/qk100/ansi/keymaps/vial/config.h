3ffe3889a861fd0a322c6481e44f0ecabd85ed16
TunaLobster <charliepjohnson@gmail.com>
2024-08-31 21:06:40 -0500

    Add initial attempt at QK100 support

---
+++ keyboards/qwertykeys/qk100/ansi/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3A, 0xD5, 0x5D, 0xBF, 0x28, 0x63, 0x17, 0x26}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
